7948f9653b5834b0ae1794e4d098e37779670fbf
Guilherme Fernandes <fernandes.guilhermelino@gmail.com>
2023-02-23 10:10:33 -0300

    Update scrap.js

--- scrap/scrap.js
+++ scrap/scrap.js
@@ -12,9 +12,8 @@ var movies = fs.createWriteStream('movies.txt', { flags: 'a' });
     await driver.get('https://tvglobo.fandom.com/pt-br/wiki/Lista_de_filmes_exibidos_na_Sess%C3%A3o_da_Tarde');
     try {
 
-        let elements = await driver.findElements(By.css('ul li'));
+        let elements = await driver.findElements(By.css('ul>li'));
         for (let e of elements) {
-
             movies.write(await e.getText() + '\n');
         }
     }
